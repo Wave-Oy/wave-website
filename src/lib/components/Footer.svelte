b95028e8b7a23d652101eac0ed2cff4de0071311
GeorgyPk <georgy.pokazeev@tak.fi>
2024-05-12 18:25:28 +0300

    Initial footer content

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,17 +1,34 @@
 <script lang="ts">
-
+    import { base } from '$app/paths';
 </script>
 
 <footer>
-    <h1>Footer content</h1>
-    <h1>Footer content</h1>
+    <div class="container">
+        <a href="{base}/" class="logo">wave</a>
+    </div>
+    <div class="container">
+        <a href="{base}/#form-section">Sign up to be the first to know!</a> <!-- TODO: need to create section with id = "form-section"-->
+    </div>
+    <div class="container">
+        <ul>
+            <li>
+                <a href="{base}/">About</a>
+            </li>
+            <li>
+                <a href="{base}/">Contact</a>
+            </li>
+            <li>
+                <a href="{base}/">For salons</a>
+            </li>
+        </ul>
+    </div>
 </footer>
 
 <style lang="scss">
     footer {
-        margin: 0;
+        margin: auto;
         padding: 10px;
-        height: 3rem;
+        height: auto;
         width: 100vw;
         background-color: var(--background-accent);
         justify-content: center;
@@ -21,9 +38,33 @@
         top: 100%;
     }
 
-    h1 {
+    .logo {
+        line-height: var(--nav-height);
+        float: left;
+        text-decoration: none;
+        font-weight: 500;
+        color: var(--foreground-on-accent);
+        font-size: 2.6rem;
+        letter-spacing: -0.05em;
+    }
+    ul {
+        list-style-type: none;
+        float: right;
+        margin: 0;
         padding: 0;
-        margin: auto;
+    }
+    li {
         text-align: center;
+        float: left;
+        display: block;
+        margin-left: 4rem;
+        font-weight: 500;
+    }
+    a {
+        line-height: var(--nav-height);
+        text-decoration: none;
+        display: block;
+        color: var(--foreground-on-accent);
+        font-size: var(--label-medium);
     }
 </style>
