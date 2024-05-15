8d6f492e0190e0b1889bc78aa0950c2b9888bfe3
topekodev <tomi@topeko.dev>
2024-05-15 20:48:42 +0300

    About page responsivity

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,55 +4,60 @@
 </script>
 
 <div class="about page">
-    <div class="row" style="max-height: 300px; margin-top:50px;">
-        <div class="col box-around">
-            <h1 class="gradient-l-r">Our job is to make hairstyle easier for everyone.</h1>
-            <p>No more endless scrolling through thousands of pictures of supermodels for you, and no more vague pictures for your barber!</p>
-        </div>
-        <div class="col" style="margin-top: 150px;">
-            <img src={`${base}/about-decorations.png`} alt="decorations" class="decorations">
-        </div>
-    </div>
-    <div class="row box-around" style="margin-top:300px; max-width:800px;">
-        <div class="col">
-            <h1 class="gradient-l-r">Our story</h1>
-            <p>
-                “Do you know what hairstyle to get? I just go to Google to scroll through thousands of pictures of hairstyles only to get frustrated and anxious!” 
-                <br>
-                <br>
-                I recall these were the exact words I said to Simo when I first had the idea of wave. We both realised on that call that before every haircut ever, we have had difficulties in selecting a good looking haircut. Essentially wave was born from a mutual need to make choosing hairstyles easier! 
-                <br>
-                <br>
-                Fast forward to today, we are a passionate team of 5 individuals, sharing the same common vision, which has guided us from the beginning. Our journey from a simple conversation to winning IdeaRace, South Karelias biggest pitching competition's student category, is a remarkable one, but just the beginning!
-                <br>
-                <br>
-                Now, we're ready for the next stage of our journey. With a clear vision and a passionate team, we are revolutionizing the hairstyling-industry one bold idea at a time! 
-                <br>
-                <br>
-                - Ville Sekki, CEO & Co-Founder
-            </p>
-        </div>
-        <div class="col headshot-container">
-            <img src={`${base}/headshot_ville.jpg`} alt="Ville Sekki" class="headshot">
-            <p class="headshot-title">Ville Sekki</p>
-        </div>
-    </div>
-    <div class="row" style="margin-top:50px;">
-        <div class="col">
-            <img src={`${base}/about-decorations.png`} alt="decorations">
-        </div>
-        <div class="col box-around" style="margin-top:250px;">
-            <h1 class="gradient-l-r">Why do we do this?</h1>
-            <p>
-                Our job is to make choosing a hairstyle easier for everyone. That's our thing. But our deeper mission is to empower individuals, like you, to discover their ideal self-expression and image.
-                <br>
-                <br>
-                With Wave, we aim to boost everyone's confidence in their appearance. Hairstyling plays a significant role in our overall look, and with Wave, we help you find the best haircut for you, regardless of your hair type. No one needs to resemble a supermodel to shine bright like a diamond!
-                <br>
-                <br>
-                Through innovative technology and personalization, we're making hairstyling accessible, convenient, and perfectly tailored to everyone!
-            </p>
-        </div>
+    <div class="container">
+        <header class="row">
+            <div class="col box-around">
+                <h1 class="gradient-l-r">Our job is to make hairstyle easier for everyone.</h1>
+                <p>No more endless scrolling through thousands of pictures of supermodels for you, and no more vague pictures for your barber!</p>
+            </div>
+            <div class="col" style="margin-top: 150px;">
+                <img src={`${base}/about-decorations.png`} alt="decorations" class="decorations">
+            </div>
+        </header>
+        <section>
+            <div class="row box-around content" style="max-width:800px;">
+                <div class="col">
+                    <h2 class="gradient-l-r">Our story</h2>
+                    <p>
+                        “Do you know what hairstyle to get? I just go to Google to scroll through thousands of pictures of hairstyles only to get frustrated and anxious!” 
+                        <br>
+                        <br>
+                        I recall these were the exact words I said to Simo when I first had the idea of wave. We both realised on that call that before every haircut ever, we have had difficulties in selecting a good looking haircut. Essentially wave was born from a mutual need to make choosing hairstyles easier! 
+                        <br>
+                        <br>
+                        Fast forward to today, we are a passionate team of 5 individuals, sharing the same common vision, which has guided us from the beginning. Our journey from a simple conversation to winning IdeaRace, South Karelias biggest pitching competition's student category, is a remarkable one, but just the beginning!
+                        <br>
+                        <br>
+                        Now, we're ready for the next stage of our journey. With a clear vision and a passionate team, we are revolutionizing the hairstyling-industry one bold idea at a time! 
+                        <br>
+                        <br>
+                        - Ville Sekki, CEO & Co-Founder
+                    </p>
+                </div>
+                <div class="col headshot-container">
+                    <img src={`${base}/headshot_ville.jpg`} alt="Ville Sekki" class="headshot">
+                    <p class="headshot-title">Ville Sekki</p>
+                </div>
+
+            </div>
+            <div class="row">
+                <div class="col">
+                    <img src={`${base}/about-decorations.png`} alt="decorations" class="decorations">
+                </div>
+                <div class="col box-around content">
+                    <h2 class="gradient-l-r">Why do we do this?</h2>
+                    <p>
+                        Our job is to make choosing a hairstyle easier for everyone. That's our thing. But our deeper mission is to empower individuals, like you, to discover their ideal self-expression and image.
+                        <br>
+                        <br>
+                        With Wave, we aim to boost everyone's confidence in their appearance. Hairstyling plays a significant role in our overall look, and with Wave, we help you find the best haircut for you, regardless of your hair type. No one needs to resemble a supermodel to shine bright like a diamond!
+                        <br>
+                        <br>
+                        Through innovative technology and personalization, we're making hairstyling accessible, convenient, and perfectly tailored to everyone!
+                    </p>
+                </div>
+            </div>
+        </section>
     </div>
 </div>
 
@@ -65,6 +70,15 @@
         flex-direction: column;
         justify-content: space-between;
         align-items: center;
+    }
+
+    header {
+        max-height: 400px;
+        margin-top:50px;
+    }
+
+    .content {
+        margin-top: 200px;
     }
 
     .row {
@@ -102,7 +116,22 @@
     }
 
     h1 {
-        font-size: 3.6rem;
+        font-size: var(--header-large);
+        font-weight: 600;
+        margin: 0;
+        padding: 0;
+
+        &.section-title {
+            font-size: 4.8rem;
+            margin-top: 2rem;
+        }
+
+        &.margin-bottom {
+            margin-bottom: 2rem;
+        }
+    }
+    h2 {
+        font-size: var(--header-medium);
         font-weight: 600;
         margin: 0;
         padding: 0;
@@ -121,7 +150,6 @@
         font-size: var(--paragraph-medium);
         color: var(--foreground-accent);
         font-weight: 500;
-        max-width: 500px;
         padding: 0;
     }
 
@@ -146,5 +174,36 @@
         border-radius: 50%;
         border: 2px solid var(--foreground-accent);
         width: 250px;
+    }
+    .headshot-container-mobile {
+        display: none;
+    }
+
+    /* Responsive view */
+    @media (max-width: 1000px) {
+        header {
+            margin-top: 25px;
+        }
+        .decorations {
+            display: none;
+        }
+        .content {
+            margin-top: 100px;
+        }
+        .col.box-around {
+            max-width: none;
+        }
+        .headshot-container {
+            display: none;
+            /* Make appear below or above the text, idc */
+        }
+    }
+    @media (max-width: 600px) {
+        h1 {
+            font-size: 8vw;
+        }
+        h2 {
+            font-size: var(--header-small);
+        }
     }
 </style>
